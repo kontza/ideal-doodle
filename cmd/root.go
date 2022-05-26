5515d1472e5c6c9fd209e71e90d1837edfe7bc65
Juha Ruotsalainen <kontza@gmail.com>
2022-05-26 08:06:15 +0300

    Added UDP-option.

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -17,24 +18,55 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "ideal-doodle",
-	Short:   "A simple Go web server. You can specify the port on command line.",
+	Short:   "A simple Go TCP/UDP. You can specify the port on command line.",
 	Version: "2.2",
 	Run: func(cmd *cobra.Command, args []string) {
 		if address, err := cmd.Flags().GetString("address"); err != nil {
 			log.Error().Err(err).Msg("Failed to get 'address' from flags:")
 		} else {
 			log.Info().Str("address", address).Msg("Current")
-			if listener, err := net.Listen("tcp4", address); err != nil {
-				log.Error().Err(err).Msg("Failed to get a listener:")
+			if useUdp, err := cmd.Flags().GetBool("udp"); err != nil {
+				log.Warn().Err(err).Msg("Failed to get 'udp' from flags:")
+				useUdp = false
 			} else {
-				defer listener.Close()
-				parts := strings.Split(listener.Addr().String(), ":")
-				log.Info().Msgf("Listening... Now do 'echo -n test | nc %s %s' in another window.", parts[0], parts[1])
-				for {
-					if conn, err := listener.Accept(); err != nil {
-						log.Fatal().Err(err).Msg("Failed to accept connection:")
+				if useUdp {
+					if pc, err := net.ListenPacket("udp4", address); err != nil {
+						log.Error().Err(err).Msg("Failed to get a packet conn:")
 					} else {
-						handleRequest(conn)
+						defer pc.Close()
+						buffer := make([]byte, 1024)
+						for {
+							n, addr, err := pc.ReadFrom(buffer)
+							if err != nil {
+								break
+							}
+							log.Info().Int("bytes", n).Str("from", addr.String()).Msg("Packet received")
+							deadline := time.Now().Add(10 * time.Second)
+							err = pc.SetWriteDeadline(deadline)
+							if err != nil {
+								break
+							}
+							n, err = pc.WriteTo(buffer[:n], addr)
+							if err != nil {
+								break
+							}
+							log.Info().Int("bytes", n).Str("to", addr.String()).Msg("Packet written")
+						}
+					}
+				} else {
+					if listener, err := net.Listen("tcp4", address); err != nil {
+						log.Error().Err(err).Msg("Failed to get a listener:")
+					} else {
+						defer listener.Close()
+						parts := strings.Split(listener.Addr().String(), ":")
+						log.Info().Msgf("Listening... Now do 'echo -n test | nc %s %s' in another window.", parts[0], parts[1])
+						for {
+							if conn, err := listener.Accept(); err != nil {
+								log.Fatal().Err(err).Msg("Failed to accept connection:")
+							} else {
+								handleRequest(conn)
+							}
+						}
 					}
 				}
 			}
@@ -73,4 +105,5 @@ func init() {
 	zerolog.TimeFieldFormat = "15:04:05.000"
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: zerolog.TimeFieldFormat})
 	rootCmd.Flags().StringP("address", "a", "localhost:7600", "specify the address and port to listen to")
+	rootCmd.Flags().BoolP("udp", "u", false, "listen to udp")
 }
